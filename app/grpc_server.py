e6cab5ae1b95a8497ef29808541c13f8d60343f0
nithishmohan <nithishmohan88@gmail.com>
2021-06-27 22:01:31 +0530

    grpc server

---
+++ app/grpc_server.py
@@ -0,0 +1,22 @@
+from concurrent import futures
+import grpc
+
+from app.generated import meter_usage_pb2_grpc
+from app.meter_usage.grpc_service import MeterUsageService
+from app.meter_usage.config import GRPC_PORT
+
+
+class GRPCServer:
+    @staticmethod
+    def run():
+        server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+        meter_usage_pb2_grpc.add_MeterUsageServiceServicer_to_server(
+            MeterUsageService(), server
+        )
+        server.add_insecure_port(f"[::]:{GRPC_PORT}")
+        server.start()
+        server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    GRPCServer.run()
--- app/main.py
+++ app/main.py
@@ -1,12 +1,43 @@
-from fastapi import FastAPI, Response
+import logging
 import uvicorn
 
+from fastapi import FastAPI, HTTPException
+from app.meter_usage.grpc_client import GRPCClient
+from http import HTTPStatus
+
+
 app = FastAPI()
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+
+logger = logging.getLogger("uvicorn.error")
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello Nithish"}
+
+
+@app.get(
+    "/meterusages",
+    name="meterusages:get-meterusages",
+    response_model_exclude_unset=True
+)
+async def meter_usage(page: int = 1, page_size: int = 25):
+    """
+
+    :type page: int
+    :type page_size: int
+    """
+    try:
+        response = GRPCClient().run(page=page, page_size=page_size)
+    except Exception as e:
+        logging.critical(e, exc_info=True)
+        raise HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail="Internal Server Error")
+
+    return response
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080)
