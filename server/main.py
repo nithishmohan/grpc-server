01d24c680fb529db7a2e564da9d114a2053020dd
nithishmohan <nithishmohan88@gmail.com>
2021-06-27 14:19:10 +0530

    Initialize fast api server

---
+++ server/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, Response
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello Nithish"}
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
